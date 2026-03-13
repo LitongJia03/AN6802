613d007a57c57837af85b6f083ac52e89f5647b8
LitongJia03 <imklicu@gmail.com>
2026-03-13 16:12:55 +0000

    Update app and remove hardcoded API key

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask,render_template,request
 import joblib
+import os
+from groq import Groq
+
 
 model = joblib.load("foodexp.pkl")
 
+client=Groq()
 app = Flask(__name__)
 
 @app.route("/",methods=["get","post"])
@@ -34,6 +38,31 @@ def foodExp():
     q = float(request.form.get("q"))
     r = model.predict([[q]])
     return(render_template("foodExp.html",r=r[0][0]))
+
+@app.route("/chatbot",methods=["get","post"])
+def chatbot():
+    return(render_template("chatbot.html"))
+
+@app.route("/roe",methods=["get","post"])
+def roe():
+    r = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[{"role":"system","content":"Please explain ROE in 20 words"}]
+    )
+    return(render_template("roe.html",r=r.choices[0].message.content))
+
+@app.route("/generalQuestion",methods=["get","post"])
+def generalQuestion():
+    return(render_template("generalQuestion.html"))
+
+@app.route("/groqReply",methods=["get","post"])
+def groqReply():
+    q = request.form.get("q")
+    r = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[{"role":"system","content":q}]
+    )
+    return(render_template("groqReply.html",r=r.choices[0].message.content))
 
 if __name__=="__main__":
     app.run()
